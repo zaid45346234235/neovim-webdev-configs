f86de54473b1561853b135cb7d5cb9d33757127c
Zaid Bash <zedalbash09123@gmail.com>
2026-04-08 11:14:52 +0300

    fix: remove autopairs

--- init.lua
+++ init.lua
@@ -19,65 +19,61 @@ if not vim.loop.fs_stat(lazypath) then
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable", lazypath,
   })
-end
-vim.opt.rtp:prepend(lazypath)
+  end
+  vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  -- LSP & Autocompletion
-  { "neovim/nvim-lspconfig" },
-  { "williamboman/mason.nvim",          config = true },
-  { "williamboman/mason-lspconfig.nvim" },
-  { "hrsh7th/nvim-cmp" },
-  { "hrsh7th/cmp-nvim-lsp" },
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = { "rafamadriz/friendly-snippets", },
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load()
-    end,
-  },
-  { "neoclide/coc.nvim",               branch = "release" },
-  { "rafamadriz/friendly-snippets" },
-  { "saadparwaiz1/cmp_luasnip" },
-  -- Colors themes
-  { "Mofiqul/vscode.nvim" },
-  -- Syntax highlighting
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-  -- Discord rich presencs or "cord.nvim"
-  { "vyfor/cord.nvim",                 bulid = ":Cord update" },
+  require("lazy").setup({
+    -- LSP & Autocompletion
+    { "neovim/nvim-lspconfig" },
+    { "williamboman/mason.nvim", config = true },
+    { "williamboman/mason-lspconfig.nvim" },
+    { "hrsh7th/nvim-cmp" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "L3MON4D3/LuaSnip", dependencies = {"rafamadriz/friendly-snippets",}, config = function()
+    require("luasnip.loaders.from_vscode").lazy_load()
+  end, },
+    { "neoclide/coc.nvim", branch = "release"},
+    { "rafamadriz/friendly-snippets" },
+    { "saadparwaiz1/cmp_luasnip" }, 
+    -- Colors themes 
+    { "Mofiqul/vscode.nvim"},
+    -- Syntax highlighting
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    -- Discord rich presencs or "cord.nvim"
+    {"vyfor/cord.nvim", bulid = ":Cord update"},
 
-  -- autotag
-  { "windwp/nvim-autopairs" },
-  { "mattn/emmet-vim" },
-  { "windwp/nvim-ts-autotag" },
-  -- quick comment plugin
-  { "numToStr/Comment.nvim" },
+    -- autotag
+  --  { "windwp/nvim-autopairs" },
+    { "mattn/emmet-vim" },
+    { "windwp/nvim-ts-autotag"},
+    -- quick comment plugin
+    { "numToStr/Comment.nvim"},
+    
+    --multiselect function
+    { "mg979/vim-visual-multi"},
+    -- Fuzzy Finder
+    { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
 
-  --multiselect function
-  { "mg979/vim-visual-multi" },
-  -- Fuzzy Finder
-  { "nvim-telescope/telescope.nvim",   dependencies = { "nvim-lua/plenary.nvim" } },
-
-  -- File Explorer & UI
-  { "nvim-tree/nvim-tree.lua" },
-  { "nvim-tree/nvim-web-devicons" },
-  { "folke/tokyonight.nvim" },
-
-  -- Formatters
-  { "mhartington/formatter.nvim" },
-  { "stevearc/conform.nvim" },
+    -- File Explorer & UI
+    { "nvim-tree/nvim-tree.lua" },
+    { "nvim-tree/nvim-web-devicons" },
+    { "folke/tokyonight.nvim" },
+    
+    -- Formatters
+    {"mhartington/formatter.nvim"},
+    {"stevearc/conform.nvim"},
 })
 
--- ========================
--- Plugins Config
--- ========================
+  -- ========================
+  -- Plugins Config
+  -- ========================
 
 
 
 -- Mason
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "ts_ls", "html", "cssls", "jsonls", "eslint", "tailwindcss-language-server" },
+  ensure_installed = { "ts_ls", "html", "cssls", "jsonls", "eslint", "tailwindcss-language-server"},
 })
 require("conform").setup({
   formatters_by_ft = {
@@ -111,24 +107,23 @@ vim.api.nvim_set_keymap("i", "<S-Tab>",
   opts)
 
 -- <CR> to confirm completion
-vim.api.nvim_set_keymap("i", "<CR>",
-  "coc#pum#visible() ? coc#pum#confirm() : \"\\<C-g>u\\<CR>\\<c-r>=coc#on_enter()\\<CR>\"", { expr = true, silent = true })
-
+vim.api.nvim_set_keymap("i", "<CR>", "coc#pum#visible() ? coc#pum#confirm() : \"\\<C-g>u\\<CR>\\<c-r>=coc#on_enter()\\<CR>\"", {expr = true, silent = true}) 
+ 
 -- Trigger completion manually
 vim.api.nvim_set_keymap("i", "<C-Space>", 'coc#refresh()', opts)
 
 -- LSP (Neovim 0.11+ API)
 -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
---
+-- 
 -- capabilities.textDocument.completion.completionItem.snippetSupport = true
---
+-- 
 -- vim.lsp.config.ts_ls = { capabilities = capabilities }
 -- vim.lsp.config.html = { capabilities = capabilities }
 -- vim.lsp.config.cssls = { capabilities = capabilities }
 -- vim.lsp.config.jsonls = { capabilities = capabilities }
 -- vim.lsp.config.eslint = { capabilities = capabilities }
 -- vim.lsp.config.tailwindcss = {capabilities = capabilities}
---
+-- 
 -- vim.lsp.enable("ts_ls")
 -- vim.lsp.enable("html")
 -- vim.lsp.enable("cssls")
@@ -146,59 +141,56 @@ vim.api.nvim_set_keymap("i", "<C-Space>", 'coc#refresh()', opts)
 --   }),
 --   sources = { { name = "nvim_lsp" } },
 -- })
---
---
+-- 
+-- 
 --   --Lsp go-to-defintion
 -- vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 -- vim.keymap.set("n", "gr", vim.lsp.buf.references)
 -- vim.keymap.set("n", "K", vim.lsp.buf.hover)
 -- vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
--- ========================
--- Treesitter
--- ========================
-require("nvim-treesitter").setup({
-  ensure_installed = { "lua", "javascript", "typescript", "tsx", "json", "css", "html" },
-  highlight = { enable = true },
-  view = {
-    mapping = {
-      list = {
-        { key = "o", action = "edit" },
-        { key = "v", action = "vsplit" },
-        { key = "s", action = "split" },
-        { key = "r", action = "rename" },
-        { key = "c", action = "copy" },
-        { key = "x", action = "cut" },
-        { key = "p", action = "paste" },
-        { key = "q", action = "close" },
-      }
-    }
-  }
+  -- ========================
+  -- Treesitter
+  -- ========================
+  require("nvim-treesitter").setup({
+    ensure_installed = { "lua", "javascript", "typescript", "tsx", "json", "css", "html" },
+    highlight = { enable = true },
+    view = {mapping = {
+    list = {
+      {key = "o", action = "edit"},
+      {key = "v", action = "vsplit"},
+      {key = "s", action = "split"},
+      {key = "r", action = "rename"},
+      {key = "c", action = "copy"},
+      {key = "x", action = "cut"},
+      {key = "p", action = "paste"},
+      {key = "q", action = "close"},
+    }}} 
+      
+  })
 
-})
+  vim.keymap.set("n", "<Leader>t",":NvimTreeToggle<CR>", {noremap = true , silent = true})
 
-vim.keymap.set("n", "<Leader>t", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+  -- ========================
+  -- autotags
+  -- ========================
 
--- ========================
--- autotags
--- ========================
+ --  require("nvim-autopairs").setup({
+ --    check_ts = true
+ --  })
+ --  
+  require("nvim-ts-autotag").setup({})
+  -- ========================
+  -- Telescope
+  -- ========================
+  local builtin = require("telescope.builtin")
+  vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+  vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 
-require("nvim-autopairs").setup({
-  check_ts = true
-})
-
-require("nvim-ts-autotag").setup({})
--- ========================
--- Telescope
--- ========================
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-
--- ========================
--- File Explorer
--- ========================
-require("nvim-tree").setup()
-vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+  -- ========================
+  -- File Explorer
+  -- ========================
+  require("nvim-tree").setup()
+  vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
 
 -- ========================
@@ -211,45 +203,45 @@ vim.cmd.colorscheme("tokyonight")
 
 -- =======================
 -- Formatter.nvim
--- =======================
+-- =======================   
 local prettier = vim.fn.expand("~/.local/share/nvim/mason/bin/prettier")
 
 require("formatter").setup({
-  logging = true, -- turn on logging to see errors
+  logging = true,  -- turn on logging to see errors
   filetype = {
     javascript = {
       function()
-        return {
-          exe = prettier,
-          args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0) },
-          stdin = true,
-        }
+      return {
+        exe = prettier,
+        args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0) },
+                           stdin = true,
+      }
       end
     },
     typescript = {
       function()
-        return {
-          exe = prettier,
-          args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0) },
-          stdin = true,
-        }
+      return {
+        exe = prettier,
+        args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0) },
+                           stdin = true,
+      }
       end
     },
     typescriptreact = {
       function()
-        return {
-          exe = prettier,
-          args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0) },
-          stdin = true,
-        }
+      return {
+        exe = prettier,
+        args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0) },
+                           stdin = true,
+      }
       end
     },
     javascriptreact = {
       function()
         return {
-          exe = prettier,
-          args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0) },
-          stdin = true,
+          exe = prettier, 
+          args = { "--stdin-filepath", vim.api.nvim_buf_get_name(0)},
+                            stdin = true,
         }
       end
     },
@@ -262,3 +254,4 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*",
   callback = function() vim.cmd("Format") end,
 })
+
